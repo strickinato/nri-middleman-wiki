9eb347fd9e8e6118c259ef4f025d790a7c1fe192
Aaron Strick <aaronstrick@gmail.com>
2015-11-16 09:24:07 -0800

    Initial commit

---
+++ config.rb
@@ -0,0 +1,88 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+# ready do
+#   binding.pry
+#   sitemap.resources.group_by {|p| p.data["category"] }.each do |category, pages|
+#     proxy "/categories/#{category}.html", "category.html",
+#       :locals => { :category => category, :pages => pages }
+#   end
+# end
+
+
+
+# For achieving Github Flavored Markdown
+set :markdown, :tables => true, :autolink => true, :gh_blockcode => true, :fenced_code_blocks => true, :smartypants => true
+set :markdown_engine, :redcarpet
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+
+  activate :syntax
+
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
---
+++ source/process/eating.md
@@ -0,0 +1,12 @@
+---
+title: eating
+---
+# hello
+
+## h2 example
+
+```ruby
+  def code(critical)
+    puts "hello"
+  end
+```
